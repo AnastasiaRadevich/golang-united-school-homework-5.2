2a945e827982ff204e8497758308bfd0ae3e829b
ARadzevich <Anastasiya_Radzevich@epam.com>
2022-05-22 21:52:19 +0400

    feat: add simple cache

--- cache.go
+++ cache.go
@@ -1,23 +1,63 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type Time struct {
+	IsExpired bool
+	Date      time.Time
+}
+
+type Item struct {
+	Value        string
+	ExpireAtTime Time
+}
 
 type Cache struct {
+	CacheItems map[string]Item
 }
 
 func NewCache() Cache {
-	return Cache{}
+	i := make(map[string]Item)
+	return Cache{CacheItems: i}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (cache Cache) Get(key string) (string, bool) {
+	cache.CheckExpiredTime(key)
+	value, ok := cache.CacheItems[key]
+	return value.Value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (cache Cache) CheckExpiredTime(key string) {
+	now := time.Now()
+	if cache.CacheItems[key].ExpireAtTime.IsExpired && !cache.CacheItems[key].ExpireAtTime.Date.After(now) {
+		cache.Delete(key)
+	}
 }
 
-func (receiver) Keys() []string {
+func (cache Cache) Delete(key string) {
+	delete(cache.CacheItems, key)
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache Cache) Put(key, value string) {
+	cache.CacheItems[key] = Item{Value: value, ExpireAtTime: Time{IsExpired: false}}
+}
+
+func (cache Cache) Keys() []string {
+	now := time.Now()
+	var keys []string
+	for key, value := range cache.CacheItems {
+		if !value.ExpireAtTime.IsExpired || value.ExpireAtTime.Date.After(now) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
+}
+
+func (cache Cache) PutTill(key, value string, deadline time.Time) {
+	now := time.Now()
+	if deadline.After(now) {
+		cache.CacheItems[key] = Item{Value: value, ExpireAtTime: Time{IsExpired: true, Date: deadline}}
+	}
 }
